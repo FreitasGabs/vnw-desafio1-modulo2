7331a6cbdd25ba9a76c028a2ed2095a375121821
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-10-23 15:32:26 +0000

    :lipstick: Estilizando o texto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 // importando o estilo
 import "./global.scss";
 
+// baseado no figma: https://www.figma.com/design/LBODsPDSyKhBv5lex4Vrsv/Untitled?node-id=1-30&node-type=frame&t=ao6p0POX5jXKAkKM-0
+// e no código: https://codesandbox.io/p/sandbox/lcfhk4
+
 //iniciando nossa função principal
 export default function LampadaMagica() {
   const [lampada, setLampada] = useState(false);
@@ -14,7 +17,6 @@ export default function LampadaMagica() {
   return (
     <main>
       <h2>"Liberte a magia da programação com a lâmpada de Alladin"</h2>
-      <h3>Clique na lâmpada e faça um pedido:</h3>
 
       <img
         src={
@@ -25,6 +27,8 @@ export default function LampadaMagica() {
         alt="Lâmpada"
         onClick={trocarLampada}
       />
+
+      <h3>Clique na Lâmpada</h3>
     </main>
   );
 }
